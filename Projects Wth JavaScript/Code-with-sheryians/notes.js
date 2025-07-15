f6a299a6245b2c5a2bd6fb9dfae9e271b2a72dd5
Mandeep-Singh-Samra <mandeep131414singh@gmail.com>
2025-07-15 18:03:41 +0530

    Update: <what you changed>

--- Projects Wth JavaScript/Code-with-sheryians/notes.js
+++ Projects Wth JavaScript/Code-with-sheryians/notes.js
@@ -19,8 +19,17 @@
 
 
     2-html ka text badlna
-      element.innerHTML = "hello world";
-      element2.innerHTML = "hello world";
+      1-innerText --> text badlda a
+      2-textConetent --> text badlda a
+      3-innerHTML --> html likh sakde aw apa ehder andr and tn krke eh html nu bdl skda aw andrle nu 
+        syntax-  
+          let hero = document.querySelector("h1");
+          hero.innerHTML = "<button>hello Mendy ki hall a</button>";
+      
+          1.ehde vich apa ne pehlan h1 nu select kr lya   te hero naam de variable de vich store kr dita ,fer apa ne hero. lake nal textContent naam ni property use kri jehde nal apa html de andr da text change kr skde aw jehda apa ne select krya hoyia a 
+
+            let hero = document.querySelector('h1');
+            hero.textContent = "hello Mendy ki hall a";
 
     3-html ko badlna
       element.style.color = "red";
@@ -48,4 +57,33 @@
       element.addEventListener("click", function () {
       console.log("clicked");
       });
+
+    2.1-attribue-  ohna nu kehnde aw apa jehde tag to bina nal appa likhe hunde aw oh attribute hunde aw  for example  <a href=""> ehde vich 'href' attribute a 
+    2.1-Attribute manipulation - apa ohnu kehnde aw jdo apa JavaScript de through attribute di value bdl dyie ja add kr dyie ja update kr dyie , ohnu bs apa attribute manipulation kehnde aw.
+    syntax -
+      let a = document.querySelector("a");
+      a.href = "https://www.google.com";
+    pro-syntax-
+      let a = document.querySelector("a");
+      a.setAttribute("href", "https://www.google.com")
+
+    pro-syntax-2-
+      let img = document.querySelector("img");
+      img.setAttribute(
+      "src",
+      "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_120x44dp.png"
+      );
+
+
+
+
+
+
+
+
+
+
+
+
+
 */
---
+++ Projects Wth JavaScript/Code-with-sheryians/script.js
@@ -0,0 +1,7 @@
+let a = document.querySelector("a");
+a.setAttribute("href", "https://www.google.com");
+let img = document.querySelector("img");
+img.setAttribute(
+  "src",
+  "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_120x44dp.png"
+);
