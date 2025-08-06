821586365d1fc1c4d3745139d64468e3ce4aea38
Mandeep-Singh-Samra <mandeep131414singh@gmail.com>
2025-08-06 16:11:04 +0530

    Update: New commit

--- Projects Wth JavaScript/Code-with-sheryians/Event-object/script.js
+++ Projects Wth JavaScript/Code-with-sheryians/Event-object/script.js
@@ -1,20 +1,15 @@
-let button = document.querySelector("#startBtn");
-let progress = document.querySelector(".progress");
-let percent = document.querySelector(".percent");
-button.addEventListener("click", function () {
-  {
-    let count = 0;
-    setInterval(function () {
-      if (count < 100) {
-        count++;
-        progress.style.width = count + "%";
-        progress.textContent = count + "%";
-        percent.textContent = count + "%";
-        console.log("Count: " + count);
-      } else {
-        console.log("Count reached 100, stopping.");
-        clearInterval(count);
-      }
-    }, 30);
+function setDarkorLight() {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.body.classList.add("dark");
+    document.body.classList.remove("light");
+  } else {
+    document.body.classList.add("light");
+    document.body.classList.remove("dark");
   }
-});
+}
+setDarkorLight();
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", function () {
+    setDarkorLight();
+  });
